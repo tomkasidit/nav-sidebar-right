39f6ccc579932a0acf225d95744dcc0c0f49d429
tomkasidit <58105132+tomkasidit@users.noreply.github.com>
2020-11-13 15:35:41 +0700

    Add files via upload

---
+++ nav-sidebar.js
@@ -0,0 +1,7 @@
+function closeNavR() {
+    document.getElementById("mySidenavR").style.width = "0";
+}
+
+function openNavR() {
+    document.getElementById("mySidenavR").style.width = "230px";
+}
---
+++ nav.php
@@ -0,0 +1,20 @@
+<!-- button open nav sidebar right -->
+
+<img id="btn-openNav" src="<?= $web_server ?>/images/add-button-b.png" onclick="openNavR()">
+
+<!-- nav sidebar right -->
+
+<div id="mySidenavR" class="sidenavR">
+
+    <button class="closebtn" onclick="closeNavR()">CLOSE <i class="arrow right"></i> </button>  
+
+    <div class="navR-header">
+        ...HEAD...
+    </div>
+
+    <div class="navR-content">
+        ...CONTENT...
+    </div>
+
+
+</div>
